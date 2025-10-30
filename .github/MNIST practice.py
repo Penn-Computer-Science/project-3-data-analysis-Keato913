a9978f8ad09fb673decf6a4f513fb4081a5d589f
Keato913 <kwatkins799@phm.k12.in.us>
2025-10-30 09:50:34 -0400

    Update MNIST practice.py

--- .github/MNIST practice.py
+++ .github/MNIST practice.py
@@ -24,3 +24,32 @@ x_train = x_train/255.0
 
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 x_test = x_test/255.0 
+
+#one_hot not sparce
+y_train = tf.one_hot(y_train.astype(np.int32), depth = 10)
+y_test = tf.one_hot(y_test.astype(np.int32), depth = 10)
+
+#Example image from MNIST
+plt.imshow(x_train[100][:,:,0])
+plt.show()
+
+
+batch_size = 128
+num_classes = 10
+epochs = 5
+
+#Build the model
+model = tf.karas.models.sequential(
+    [
+    tf.karas.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape),
+    tf.karas.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    tf.karas.layers.maxpool2D(),
+    tf.karas.layers.dropout[0,25],
+    tf.karas.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    tf.karas.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    tf.karas.layers.Dense(num_classes, activation='sotmax')
+
+    ]
+)
+
+model.comp(optimizer=)
