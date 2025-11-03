718281f35db0a0ee494867555b86560ed23d2d98
Keato913 <kwatkins799@phm.k12.in.us>
2025-11-03 09:51:27 -0500

    Update MNIST practice.py

--- .github/MNIST practice.py
+++ .github/MNIST practice.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import random
 
 print(tf.__version__)
 
@@ -30,7 +31,7 @@ y_train = tf.one_hot(y_train.astype(np.int32), depth = 10)
 y_test = tf.one_hot(y_test.astype(np.int32), depth = 10)
 
 #Example image from MNIST
-plt.imshow(x_train[100][:,:,0])
+plt.imshow(x_train[random.randint(0,59999)][:,:,0], cmap = 'grey')
 plt.show()
 
 
@@ -39,17 +40,36 @@ num_classes = 10
 epochs = 5
 
 #Build the model
-model = tf.karas.models.sequential(
+model = tf.keras.models.Sequential(
     [
-    tf.karas.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape),
-    tf.karas.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=input_shape),
-    tf.karas.layers.maxpool2D(),
-    tf.karas.layers.dropout[0,25],
-    tf.karas.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
-    tf.karas.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
-    tf.karas.layers.Dense(num_classes, activation='sotmax')
+    tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape),
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Dropout(0,25),
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(num_classes, activation='softmax')
 
     ]
 )
 
-model.comp(optimizer=)
+model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08),loss='categorical_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train, batch_size = batch_size, epochs = epochs, validation_data=(x_test, y_test))
+
+#plot out training and validation accuracy and loss
+fig, ax = plt.subplots(2,1)
+
+ax[0].plot(history.history('loss'), color = 'b', label = "Training Loss")
+ax[0].plot(history.history('val_loss'), color = 'r', label = "Training Loss")
+Legend = ax[0].Legend(loc='best', shadow=True)
+ax[0].settitle('Loss')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('Loss')
+
+ax[1].plot(history.history('Acc'), color = 'b', label = "Training Accuracy")
+ax[1].plot(history.history('val_Acc'), color = 'r', label = "Training Accuracy")
+Legend = ax[1].Legend(loc='best', shadow=True)
+ax[1].settitle('Accuracy')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Accuracy')
